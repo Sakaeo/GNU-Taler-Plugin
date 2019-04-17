75f6fded4dcfca7890d62c9f2815dec9eedb3ff3
Jan <jan@struebin.com>
2019-04-17 22:21:07 +0200

    Basic stuff update

--- src/GNU-taler-payment/GNU-taler-payment.php
+++ src/GNU-taler-payment/GNU-taler-payment.php
@@ -3,27 +3,36 @@
  * @package GNUTalerPayment
  */
 /**
- * Plugin Name: GNU Taler Payment
+ * Plugin Name: GNU Taler Payment for Woocommerce
  * Plugin URI: https://github.com/Sakaeo/GNU-Taler-Plugin
+ *      //Or Wordpress pluin URI
  * Description: This plugin enables the payment via the GNU Taler payment system
  * Version: 1.0.0
  * Author: Hoffman Dominique & Strübin Jan
  * Author URI: https://i.pinimg.com/originals/75/eb/90/75eb90f3e667aa24a514b801e3b96a54.jpg
- * License: GPLv2 or later
+ *      //TBD
+ *
+ * License:           GNU General Public License v3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+ * WC requires at least: 2.2
  **/
 
 /*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {exit;}
+
+// TODO: If Woocommerce active -> Code
