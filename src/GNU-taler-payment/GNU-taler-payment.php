ca028790fd3863e1bac9f32aad16e693adfceefa
Jan <jan@struebin.com>
2019-04-17 21:36:29 +0200

    Base

---
+++ src/GNU-taler-payment/GNU-taler-payment.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package GNUTalerPayment
+ */
+/**
+ * Plugin Name: GNU Taler Payment
+ * Plugin URI: https://github.com/Sakaeo/GNU-Taler-Plugin
+ * Description: This plugin enables the payment via the GNU Taler payment system
+ * Version: 1.0.0
+ * Author: Hoffman Dominique & Strübin Jan
+ * Author URI: https://i.pinimg.com/originals/75/eb/90/75eb90f3e667aa24a514b801e3b96a54.jpg
+ * License: GPLv2 or later
+ **/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
