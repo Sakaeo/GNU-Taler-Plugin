2464b8754e861db7bae2e69d0f6a09d6ee686078
Dominique Hofmann <hofmann.dominique@bluewin.ch>
2019-05-20 10:24:54 +0200

    ConvertJson function finished Refund on good progress

--- src/GNU-taler-payment/GNU-taler-payment.php
+++ src/GNU-taler-payment/GNU-taler-payment.php
@@ -53,31 +53,36 @@ add_filter( 'woocommerce_payment_gateways', 'gnutaler_add_gateway_class' );
  * The class itself, please note that it is inside plugins_loaded action hook
  */
 add_action( 'plugins_loaded', 'gnutaler_init_gateway_class' );
-function gnutaler_init_gateway_class() {
+function gnutaler_init_gateway_class()
+{
+
+
 
     //Check if WooCommerce is active, if not then deactivate and show error message
-    if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' )))) {
-        deactivate_plugins(plugin_basename(__FILE__) );
-        wp_die( "<strong>GNU Taler</strong> requires <strong>WooCommerce</strong> plugin to work normally. Please activate it or install it from <a href=\"http://wordpress.org/plugins/woocommerce/\" target=\"_blank\">here</a>.<br /><br />Back to the WordPress <a href='".get_admin_url(null, 'plugins.php')."'>Plugins page</a>." );
+    if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+        deactivate_plugins(plugin_basename(__FILE__));
+        wp_die("<strong>GNU Taler</strong> requires <strong>WooCommerce</strong> plugin to work normally. Please activate it or install it from <a href=\"http://wordpress.org/plugins/woocommerce/\" target=\"_blank\">here</a>.<br /><br />Back to the WordPress <a href='" . get_admin_url(null, 'plugins.php') . "'>Plugins page</a>.");
     }
 
 
-    class WC_GNUTaler_Gateway extends WC_Payment_Gateway {
+    class WC_GNUTaler_Gateway extends WC_Payment_Gateway
+    {
 
         /**
          * Class constructor, more about it in Step 3
          */
-        public function __construct() {
+        public function __construct()
+        {
             $this->id = 'gnutaler'; // payment gateway plugin ID
             $this->icon = ''; // URL of the icon that will be displayed on checkout page near your gateway name
-            $this->has_fields = true; // in case you need a custom credit card form
+            $this->has_fields = false; // in case you need a custom credit card form
             $this->method_title = 'GNU Taler Gateway';
             $this->method_description = 'This plugin enables the payment via the GNU Taler payment system'; // will be displayed on the options page
 
             // gateways can support subscriptions, refunds, saved payment methods,
             // but in this tutorial we begin with simple payments
             $this->supports = array(
-                'products'
+                'products', 'refunds'
             );
 
             // Method with all the options fields
@@ -85,21 +90,23 @@ function gnutaler_init_gateway_class() {
 
             // Load the settings.
             $this->init_settings();
-            $this->title = $this->get_option( 'title' );
-            $this->description = $this->get_option( 'description' );
-            $this->enabled = $this->get_option( 'enabled' );
-            $this->testmode = 'yes' === $this->get_option( 'testmode' );
-            $this->GNU_Taler_Backend_URL = $this->get_option( 'GNU_Taler_Backend_URL' );
+            $this->title = $this->get_option('title');
+            $this->description = $this->get_option('description');
+            $this->enabled = $this->get_option('enabled');
+            $this->testmode = 'yes' === $this->get_option('testmode');
+            $this->GNU_Taler_Backend_URL = $this->get_option('GNU_Taler_Backend_URL');
+            $this->Fulfillment_url = $this->get_option('Fulfillment_url');
+            $this->Order_text = $this->get_option('Order_text');
 
             //Verify if the GNU Taler Backend URL in the settings is valid or not
             $result = $this->verifyBackendURL($this->get_option("GNU_Taler_Backend_URL", 1));
 
-                // This action hook saves the settings
+            // This action hook saves the settings
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
 
 
             // We need custom JavaScript to obtain a token
-            add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
+            add_action('wp_enqueue_scripts', array($this, 'payment_scripts'));
 
             // You can also register a webhook here
             // add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) );
@@ -108,42 +115,57 @@ function gnutaler_init_gateway_class() {
         /**
          * Plugin options, we deal with it in Step 3 too
          */
-        public function init_form_fields(){
+        public function init_form_fields()
+        {
             $this->form_fields = array(
                 'enabled' => array(
-                    'title'       => 'Enable/Disable',
-                    'label'       => 'Enable GNU Taler Gateway',
-                    'type'        => 'checkbox',
+                    'title' => 'Enable/Disable',
+                    'label' => 'Enable GNU Taler Gateway',
+                    'type' => 'checkbox',
                     'description' => '',
-                    'default'     => 'no'
+                    'default' => 'no'
                 ),
                 'title' => array(
-                    'title'       => 'Title',
-                    'type'        => 'text',
+                    'title' => 'Title',
+                    'type' => 'text',
                     'description' => 'This controls the title which the user sees during checkout.',
-                    'default'     => 'GNU Taler',
-                    'desc_tip'    => true,
+                    'default' => 'GNU Taler',
+                    'desc_tip' => true,
                 ),
                 'description' => array(
-                    'title'       => 'Description',
-                    'type'        => 'textarea',
+                    'title' => 'Description',
+                    'type' => 'textarea',
                     'description' => 'This controls the description which the user sees during checkout.',
-                    'default'     => 'Pay with the new Payment system GNU Taler.',
+                    'default' => 'Pay with the new Payment system GNU Taler.',
                 ),
                 'testmode' => array(
-                    'title'       => 'Test mode',
-                    'label'       => 'Enable Test Mode',
-                    'type'        => 'checkbox',
+                    'title' => 'Test mode',
+                    'label' => 'Enable Test Mode',
+                    'type' => 'checkbox',
                     'description' => 'Place the payment gateway in test mode using test API keys.',
-                    'default'     => 'yes',
-                    'desc_tip'    => true,
+                    'default' => 'yes',
+                    'desc_tip' => true,
                 ),
 
                 'GNU_Taler_Backend_URL' => array(
-                    'title'       => 'GNU Taler Backend URL',
-                    'type'        => 'text',
+                    'title' => 'GNU Taler Backend URL',
+                    'type' => 'text',
                     'description' => 'Set the URL for the GNU Taler Backend.',
-                    'default'     => 'https://backend.demo.taler.net',
+                    'default' => 'https://backend.demo.taler.net',
+                ),
+
+                'Fulfillment_url' => array(
+                    'title' => 'GNU Taler Fulfillment URL',
+                    'type' => 'text',
+                    'description' => 'Set the URL where the user should return after finishing the payment process.',
+                    'default' => '',
+                ),
+
+                'Order_text' => array(
+                    'title' => 'Summarytext of the order',
+                    'type' => 'text',
+                    'description' => 'Set the text the customer should see as a summary when he confirms the payment.',
+                    'default' => 'Order',
                 ),
             );
         }
@@ -152,7 +174,8 @@ function gnutaler_init_gateway_class() {
         /*
          * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
          */
-        public function payment_scripts() {
+        public function payment_scripts()
+        {
 
         }
 
@@ -160,17 +183,21 @@ function gnutaler_init_gateway_class() {
          * Method for calling REST-API
          */
 
-        function callAPI($method, $url, $body, $purpose){
+        function callAPI($method, $url, $body, $purpose)
+        {
 
-            if ($purpose == "create_order"){
+            if ($purpose == "create_order") {
                 $url = $url . "/order";
-            }
-            else if ($purpose == "confirm_payment"){
+            } else if ($purpose == "confirm_payment") {
                 $url = $url . "/check-payment?order_id=" . $body;
+            } else if ($purpose == "create_refund"){
+                $url = $url . "/refund";
+            } else if ($purpose == "confirm_refund"){
+
             }
             $curl = curl_init();
 
-            switch ($method){
+            switch ($method) {
                 case "POST":
                     curl_setopt($curl, CURLOPT_POST, 1);
                     if ($body) {
@@ -185,7 +212,7 @@ function gnutaler_init_gateway_class() {
                     break;
                 case "Simple":
                     curl_setopt($curl, CURLOPT_VERBOSE, 1);
-                   // curl_setopt($curl, CURLOPT_HEADER, 1);
+                    // curl_setopt($curl, CURLOPT_HEADER, 1);
                     break;
             }
 
@@ -214,7 +241,8 @@ function gnutaler_init_gateway_class() {
          *  Here we verify if the GNU Taler Backend URL, given in the settings, is valid.
          */
 
-        public function verifyBackendURL($String){
+        public function verifyBackendURL($String)
+        {
             return false;
         }
 
@@ -222,61 +250,60 @@ function gnutaler_init_gateway_class() {
          * Convert the order to the GNU Taler JSON-format for sending to the backend
          */
 
+
         public function convertToJSON($order_id){
             $wcorder = wc_get_order( $order_id );
             $wc_order_total_amount = $wcorder->get_total();
             $wc_order_curreny = $wcorder->get_currency();
-            $wc_order_products = $wcorder->get_items();
-            return $wc_order_curreny . ":" . $wc_order_total_amount;
+            $wc_cart = WC()->cart->get_cart();
+            $wc_order_id = $wcorder->get_order_key() . "_" . $wcorder->get_order_number();
+
+            $wc_order_products_array = array();
+            foreach ($wc_cart as $product){
+                $wc_order_products_array[] = array(
+                    'description' => "Order of product: " . $product['data']->get_title(),
+                    'quantity' => $product['quantity'],
+                    'price' => $wc_order_curreny . ":" . $product['data']->get_price(),
+                    'product_id' => $product['data']->get_id(),
+                );
+
+            }
+
+            $order_json = array(
+                "order" => array(
+                    "amount" => $wc_order_curreny . ":" . $wc_order_total_amount,
+                    //"amount" => "KUDOS:0.1",
+                    "summary" => "Order of the following items:",
+                    "fulfillment_url" => "http://gnutaler.hofmd.ch",
+                    "order_id" => $wc_order_id,
+                    //"merchant" => array(),
+                    "products" => $wc_order_products_array
+                )
+            );
+
+            return $order_json;
         }
+
+
         /*
          * We're processing the payments here
          */
-        public function process_payment( $order_id ) {
-            global $woocommerce;
+        public function process_payment($order_id)
+        {
+
 
             // we need it to get any order detailes
             $wcorder = wc_get_order( $order_id );
+            $cart = WC()->cart->get_cart();
 
             // Gets the url of the backend from the WooCommerce Settings
             $backendURL = $this->get_option("GNU_Taler_Backend_URL", 1);
 
-            //HOLY JSON FORMAT!!!!
-            $data_array =  array(
-                "order" => array(
-                    "amount" =>  "KUDOS:10",
-                    "summary" => "" ,
-                    "fulfillment_url" => "http://gnutaler.hofmd.ch/thank-you-for-your-purchase/",
-                    "products" => array(
-                            array('description' =>
-                            "Donation to charity program",
-                            'quantity' => 1,
-                            'price' =>
-                                '25',
-                            'product_id' => 0,
-                            'delivery_date' =>
-                                "/Date( 12-1-1996 )/",
-                            'delivery_location' =>
-                                'LNAME1'
-                            ),
-                            array('description' =>
-                                "Donation to charity program",
-                                'quantity' => 1,
-                                'price' =>
-                                    '25',
-                                'product_id' => 0,
-                                'delivery_date' =>
-                                    "/Date( 12-1-1996 )/",
-                                'delivery_location' =>
-                                    'LNAME1'
-                            )
-                        )
-                    )
-                );
-            //DO NOT TOUCH
+            $order_json = $this->convertToJSON($order_id);
+
 
             // Send the POST-Request via CURL to the GNU Taler Backend
-            $order_confirmation = $this->callAPI('POST', $backendURL, json_encode($data_array, JSON_UNESCAPED_SLASHES), "create_order");
+            $order_confirmation = $this->callAPI('POST', $backendURL, json_encode($order_json, JSON_UNESCAPED_SLASHES), "create_order");
             $order_confirmation_id = explode("\"", $order_confirmation)[3];
 
 
@@ -284,20 +311,49 @@ function gnutaler_init_gateway_class() {
             $payment_confirmation = $this->callAPI("Simple", $backendURL, $order_confirmation_id, "confirm_payment");
             $payment_confirmation_url = explode("\"", $payment_confirmation)[3];
 
-            //wc_add_notice($payment_confirmation_url, "success");
 
-            $wcorder->payment_complete();
-            $woocommerce->cart->empty_cart();
+            //$wcorder->payment_complete();
+            //WC()->cart->empty_cart();
 
-            // Return thankyou redirect
+            //wc_add_notice($order_confirmation, "success");
+
+
             return array(
                 'result'    => 'success',
                 'redirect'  => $payment_confirmation_url
             );
+
+
+
         }
 
+        public function process_refund($order_id, $amount = null, $reason = '') {
 
+            $wcorder = wc_get_order($order_id);
 
+            $refund_json = array(
+                "order_id" => $wcorder->get_order_number() . "_" . $wcorder->get_order_key(),
+                "refund" => $amount,
+                "instance" => "default",
+                "reason" => $reason
+            );
+
+            // Gets the url of the backend from the WooCommerce Settings
+            $backendURL = $this->get_option("GNU_Taler_Backend_URL", 1);
+
+            $wc_order_status = $wcorder->get_status();
+
+            if ($wc_order_status == "processing"  || $wc_order_status == "on hold" || $wc_order_status == "completed" || $wc_order_status == "refunded") {
+                //$redirect_confirmation = $this->callAPI("POST", $backendURL, json_encode("", JSON_UNESCAPED_SLASHES), "create_refund");
+                $wcorder->update_status("refunded");
+                return true;
+            }
+            else{
+                $wcorder->add_order_note("The refund process failed");
+                return false;
+            }
+        }
     }
+
 
 }
